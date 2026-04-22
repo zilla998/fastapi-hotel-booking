02d7cf49c310a873df8fe56e6f4d8f4b4b42e3a6
zilla998 <coldssielenssams@gmail.com>
2026-04-22 11:09:19 +0300

    refactor: add type annotations and refactor `AuthService` constructor to support dependency injection

--- src/services/auth.py
+++ src/services/auth.py
@@ -4,22 +4,25 @@ from src.config import security
 
 
 class AuthService:
-    password_hash = PasswordHash.recommended()
+    def __init__(
+        self, password_hash: PasswordHash = PasswordHash.recommended()
+    ) -> None:
+        self.password_hash = password_hash
 
-    def verify_password(self, plain_password, hashed_password):
+    def verify_password(self, plain_password, hashed_password) -> bool:
         return self.password_hash.verify(plain_password, hashed_password)
 
-    def get_password_hash(self, password):
+    def get_password_hash(self, password) -> str:
         return self.password_hash.hash(password)
 
     @staticmethod
-    def create_access_token(user_id: int):
+    def create_access_token(user_id: int) -> str:
         access_token = security.create_access_token(
             uid=str(user_id), data={"user_id": user_id}
         )
         return access_token
 
     @staticmethod
-    def create_refresh_token(user_id: int):
+    def create_refresh_token(user_id: int) -> str:
         refresh_token = security.create_refresh_token(uid=str(user_id))
         return refresh_token
