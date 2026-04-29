b7036892da90bad16429264386e509205076eab1
zilla998 <coldssielenssams@gmail.com>
2026-04-29 12:27:12 +0300

    feat: enforce strict email constraints by adding non-null & indexed email field in users model; update migration script accordingly

--- src/models/users.py
+++ src/models/users.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import String
+from sqlalchemy import DateTime, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -11,7 +11,9 @@ class UsersOrm(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(200), unique=True)
+    email: Mapped[str] = mapped_column(
+        String(200), unique=True, index=True, nullable=False
+    )
     hashed_password: Mapped[str] = mapped_column(String(200), nullable=False)
     role: Mapped[str] = mapped_column(default=UserRoles.user, nullable=False)
     is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
