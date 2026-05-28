36d181d529669453f8277970eef1f2366f4fee0e
zilla998 <coldssielenssams@gmail.com>
2026-05-28 13:40:36 +0300

    Format repository query and user schema

--- src/repositories/facilities.py
+++ src/repositories/facilities.py
@@ -14,10 +14,6 @@ class FacilitiesRepository(BaseRepository):
         return result.scalars().all()
 
     async def get_one_or_none(self, **filter_by):
-        query = (
-            select(self.model)
-            .filter_by(**filter_by)
-            .options(selectinload(self.model.rooms))
-        )
+        query = select(self.model).filter_by(**filter_by).options(selectinload(self.model.rooms))
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,4 +1,3 @@
-
 from pydantic import BaseModel, ConfigDict, EmailStr, Field, model_validator
 
 PASSWORD_MIN_LENGTH = 8
